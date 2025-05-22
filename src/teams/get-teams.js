50e22c9bfaab4432445b9c0e009ea6eeda0ebbba
Santiago Borja Romero <santiago@callevo.com>
2025-05-22 02:08:00 +0000

    teams

--- src/teams/get-teams.js
+++ src/teams/get-teams.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const {saveResponse} = require('../../util/function')
+const {saveResponse, getFileNameMethod} = require('../../util/function')
 var request = require('request');
 const path = require('path');
-filename = path.basename(__filename).split(".");
+const [current_file_name, current_file_method] = getFileNameMethod(path.basename(__filename));
 
 /**
  * Variables
@@ -21,10 +21,12 @@ let params = null
  * Process
  */
 
-request.get({url: url,headers: headers},
+request.get({url: url, json: params, headers: headers},
     function (error, response, body) {
         console.log(body)
-        if(response.statusCode == 200){
-            saveResponse(filename[0],(filename[0].split("-")[0]).toUpperCase(), url, headers, params, body);
+        if (response.statusCode == 200) {
+            saveResponse(current_file_name, current_file_method, url, headers, params, body);
+        } else {
+            console.log(error)
         }
     });         
---
+++ src/teams/put-team-id.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+const { saveResponse, getFileNameMethod } = require('../../util/function')
+var request = require('request');
+const path = require('path');
+const [current_file_name, current_file_method] = getFileNameMethod(path.basename(__filename));
+
+/**
+ * Variables
+ */
+
+let url = `${process.env.API_PATH}team/10218`
+
+let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${process.env.APP_TOKEN}`
+}
+
+let params = {
+    "queue_id": 10218,
+    "record": { 
+        "name": "TeamNodes",
+        "team_name": "TeamNodes",
+     },
+    "queue_member_table": [
+        {
+            "agentid": 15337,    
+            "queue_name": "TeamNodes",
+            "interface": "Agent/u282xdaw7ahwzfz5gsht",
+        },
+        {
+            "agentid": 15338,    
+            "queue_name": "TeamNodes",
+            "interface": "Agent/u282ms5xpeg5m696zckb",
+        },
+        {
+            "agentid": 15339,    
+            "queue_name": "TeamNode",
+            "interface": "Agent/u2822htb7mp0d8rnehrd",
+        },
+    ]
+}
+
+/**
+ * Process
+ */
+request.put({url: url, json: params, headers: headers},
+    function (error, response, body) {
+        let resp = JSON.stringify(body)
+        console.log(resp)
+        if (response.statusCode == 200) {
+            saveResponse(current_file_name, current_file_method, url, headers, params, resp);
+        } else {
+            console.log(error)
+        }
+    });         
